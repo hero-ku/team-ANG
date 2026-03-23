228f40a6ca3f10cd76cd52d40de5d7a42abe3ee3
gaurav <gaurav.paneru04@gmail.com>
2026-03-22 23:28:30 -0500

    Added Main.java for program entry point

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import game.Game;
+
+// Program entry point
+
+public class Main {
+    /*
+     Starts the chess game
+     */
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+    }
+}
