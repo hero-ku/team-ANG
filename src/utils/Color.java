9899cb24c468283815ba48eeae62ac98065f5fe0
Andrew Lynch <a_t_lynch@protonmail.ch>
2026-03-25 21:34:22 -0500

    Document Color.java

--- src/utils/Color.java
+++ src/utils/Color.java
@@ -1,10 +1,17 @@
 package utils;
 
+/**
+ * Represents one of the colors a player can be, black or white.
+ */
 public enum Color {
     WHITE,
     BLACK,
     ;
 
+    /**
+     * Returns the opposite color.
+     * @return Black if the color is white, white if the color is black.
+     */
     public Color opposite() {
         return this == WHITE ? BLACK : WHITE;
     }
